25f2133f2a1bd1b709cd869f70d6d3a7c46bfb66
omarmansor <omarmansor@gmail.com>
2022-12-15 22:55:08 +0400

    Lastest build

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker ps | xargs sudo docker rm -f
-sudo docker system prune
-sudo docker build -t omarmansor/phpwebsite:$BUILD_NUMBER .
-sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-sudo docker push omarmansor/phpwebsite:$BUILD_NUMBER
-sudo docker run -itd -p 8080:80 omarmansor/phpwebsite:$BUILD_NUMBER
